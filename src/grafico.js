aed3b6a0cd5f8363f623232c32bde95831acb4d7
eduardo <ealoisio16@gmail.comgit config --global user.email ealoisio16@gmail.comgit config --global user.email ealoisio16@gmail.com>
2025-04-16 21:00:13 -0300

    correção bug 1.0

--- src/grafico.js
+++ src/grafico.js
@@ -1,34 +1,36 @@
-import * as echarts from 'echarts';
-import { registrarComponente, obterDadosFiltrados } from './filtro';
+// Assume que ECharts já foi carregado via <script src="...echarts.min.js">
+(function () {
+  function criarGrafico(el, tipo, parametro, grupo, dados) {
+    const chart = echarts.init(el);
 
-export function criarGrafico(el, tipo, parametro, grupo, dados) {
-  const chart = echarts.init(el);
+    const atualizar = () => {
+      const filtrados = window.obterDadosFiltrados(grupo, dados);
+      const contagem = {};
+      filtrados.forEach(item => {
+        const chave = item[parametro];
+        contagem[chave] = (contagem[chave] || 0) + 1;
+      });
 
-  const atualizar = () => {
-    const filtrados = obterDadosFiltrados(grupo, dados);
-    const contagem = {};
-    filtrados.forEach(item => {
-      const chave = item[parametro];
-      contagem[chave] = (contagem[chave] || 0) + 1;
+      chart.setOption({
+        title: { text: parametro },
+        tooltip: {},
+        xAxis: { type: 'category', data: Object.keys(contagem) },
+        yAxis: { type: 'value' },
+        series: [{
+          type: tipo,
+          data: Object.values(contagem),
+        }]
+      });
+    };
+
+    chart.on('click', (params) => {
+      const valor = params.name;
+      window.registrarComponente(grupo, parametro, valor);
     });
 
-    chart.setOption({
-      title: { text: parametro },
-      tooltip: {},
-      xAxis: { type: 'category', data: Object.keys(contagem) },
-      yAxis: { type: 'value' },
-      series: [{
-        type: tipo,
-        data: Object.values(contagem),
-      }]
-    });
-  };
+    window.registrarComponente(grupo, parametro, null, atualizar);
+    atualizar();
+  }
 
-  chart.on('click', (params) => {
-    const valor = params.name;
-    registrarComponente(grupo, parametro, valor);
-  });
-
-  registrarComponente(grupo, parametro, null, atualizar);
-  atualizar();
-}
+  window.criarGrafico = criarGrafico;
+})();
