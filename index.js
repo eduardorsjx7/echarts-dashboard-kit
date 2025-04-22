348f9db6e7537b8d5de55a2c609ef951d8d4823d
eduardo <ealoisio16@gmail.comgit config --global user.email ealoisio16@gmail.comgit config --global user.email ealoisio16@gmail.com>
2025-04-21 23:13:57 -0300

    Correção do npm

---
+++ index.js
@@ -0,0 +1,17 @@
+import { criarGrafico } from './src/grafico.js';
+import { criarTabela } from './src/tabela.js';
+import {
+  registrarComponente,
+  obterDadosFiltrados,
+  limparFiltros,
+  filtros
+} from './src/filtro.js';
+
+export {
+  criarGrafico,
+  criarTabela,
+  registrarComponente,
+  obterDadosFiltrados,
+  limparFiltros,
+  filtros
+};
